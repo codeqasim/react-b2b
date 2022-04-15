693d087935db5262127dd42733abbb8c209ffb52
AbdulkadirJS <98168821+AbdulkadirJS@users.noreply.github.com>
2022-04-15 20:27:45 +0500

    push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
