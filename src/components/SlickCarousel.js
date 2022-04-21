30f2fa9aa2f6402894975d58af03d3693ef4366e
AbdulkadirJS <98168821+AbdulkadirJS@users.noreply.github.com>
2022-04-22 03:13:10 +0500

    push

--- src/components/SlickCarousel.js
+++ src/components/SlickCarousel.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import {Box} from '@mui/material/';
+import {Box,Typography,Button} from '@mui/material/';
 
 export default class SimpleSlider extends Component {
   render() {
@@ -10,14 +10,40 @@ export default class SimpleSlider extends Component {
     };
     return (
       <>
-        <h2> Single Item</h2>
         <Slider {...settings}>
-         <Box>
-         <img  src="/assets/img/home2.jpeg" height='auto' width='100%px'/>
+         <Box mt={3} borderRadius='10px'>
+           <Box sx={{ position:'relative' }}>
+           <img className="borderRadius" src="/assets/img/home1.jpeg" height='auto' width='100%px' />
+           </Box>
+         <Box ml={5} sx={{ position:'absolute',top:'160px' }}>
+         <Typography fontWeight='bold' lineHeight='25px' fontSize='17px' color='#222222' component="div">
+         Farm Fresh Veggies
+      </Typography>
+      <Typography gutterBottom fontWeight='400' lineHeight='23px' fontSize='15px' color='#777777' component="div">
+      Get instant delivery
+      </Typography>
+      <Button variant="text" sx={{ bgcolor:'#0baf9a', px:'20px', color:'white',  "&:hover": {
+      bgcolor:'#0baf9a',
+    }, }}>Shop Now</Button>
          </Box>
-         <Box>
-         <img  src="/assets/img/home1.jpeg" height='auto' width='100%px'/>
          </Box>
+         <Box borderRadius='10px' mt={3}>
+           <Box sx={{ position:'relative' }}>
+           <img className="borderRadius" src="/assets/img/home2.jpeg" height='auto' width='100%px' />
+           </Box>
+         <Box ml={5} sx={{ position:'absolute',top:'160px' }}>
+         <Typography fontWeight='bold' lineHeight='25px' fontSize='17px' color='#ffffff' component="div">
+         Farm Fresh Veggies
+      </Typography>
+      <Typography gutterBottom fontWeight='400' lineHeight='23px' fontSize='15px' color='#ffffff' component="div">
+      Get instant delivery
+      </Typography>
+      <Button variant="text" sx={{ px:'20px', bgcolor:'white', color:'#0baf9a',  "&:hover": {
+      bgcolor:'white',
+    }, }}>Shop Now</Button>
+         </Box>
+         </Box>
+         
         </Slider>
       </>
     );
